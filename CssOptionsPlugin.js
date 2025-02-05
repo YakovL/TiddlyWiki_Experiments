36856f906c8fdfa30f1489f45ae5156e531f74b2
Yakov Litvin <Yakov.A.Litvin@gmail.com>
2025-02-05 10:47:36 +0300

    feature(CssOptionsPlugin): calc styles onSystemSettingsChange, including on startup

--- CssOptionsPlugin.js
+++ CssOptionsPlugin.js
@@ -1,6 +1,7 @@
 /***
 |Description|Supports css- options: per cssSomeOption, creates and updates css variable someOption (to be used as {{{var(--someOption)}}})|
 |Source     |https://github.com/YakovL/TiddlyWiki_Experiments/blob/master/CssOptionsPlugin.js|
+|Version    |0.3.2|
 |Author     |Yakov Litvin|
 |License    |[[MIT|https://github.com/YakovL/TiddlyWiki_YL_ExtensionsCollection/blob/master/Common%20License%20(MIT)]]|
 ***/
@@ -33,4 +34,26 @@ config.macros.option.types[cssOptionPrefix].onChange = function() {
 }
 config.optionHandlers[cssOptionPrefix] = config.optionHandlers.txt
 loadOptions() // only needed in a plugin since loadOptions() is called before loadPlugins()
+
+TiddlyWiki.prototype.addOrReplaceNotification = TiddlyWiki.prototype.addOrReplaceNotification ||
+function(title, fn, newTitle, keepExisting) {
+	var existingNotification = this.namedNotifications.find(n => n.name == title)
+	if(!existingNotification) this.namedNotifications.push({ name: title, notify: fn });
+	else if(!keepExisting) {
+		existingNotification.notify = fn;
+		existingNotification.name = newTitle || title;
+	}
+	return this; // support chaining
+}
+
+if(!window.orig_onSystemSettingsChange) {
+	window.orig_onSystemSettingsChange = onSystemSettingsChange
+	onSystemSettingsChange = function() {
+		var result = orig_onSystemSettingsChange.apply(this, arguments)
+		applyVariablesCss()
+		return result
+	}
+	config.notifyTiddlers.find(notif => notif.name == "SystemSettings").notify = onSystemSettingsChange
+	store.addOrReplaceNotification("SystemSettings", onSystemSettingsChange)
+}
 //}}}
