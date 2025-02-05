146e8f79787ac89778afbbba6c3722cc258554e5
Yakov Litvin <Yakov.A.Litvin@gmail.com>
2025-02-05 10:20:22 +0300

    infra(CssOptionsPlugin): plugin skeleton

---
+++ CssOptionsPlugin.js
@@ -0,0 +1,8 @@
+/***
+|Description|Supports css- options: per cssSomeOption, creates and updates css variable someOption (to be used as {{{var(--someOption)}}})|
+|Source     |https://github.com/YakovL/TiddlyWiki_Experiments/blob/master/CssOptionsPlugin.js|
+|Author     |Yakov Litvin|
+|License    |[[MIT|https://github.com/YakovL/TiddlyWiki_YL_ExtensionsCollection/blob/master/Common%20License%20(MIT)]]|
+***/
+//{{{
+//}}}
